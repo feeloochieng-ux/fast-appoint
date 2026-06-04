5ecf44cf0c1420edf8e87c90bf9d6c65bdea637a
feeloochieng-ux <feeloochieng@gmail.com>
2026-06-04 17:12:34 +0300

    made list1 active

--- js/script.js
+++ js/script.js
@@ -2,17 +2,79 @@ const selectlist = document.querySelectorAll(".list1");
 
 const serviceCategories = document.querySelectorAll(".sect");
 
- for(let list of selectlist){
-if (list.)
-//  }
-// for (let tab of selectTabs) {
-//   if (tab.textContent.toLowerCase() === "chicken pizza") {
-//     tab.classList.add("active");
+for (let list of selectlist) {
+  if (list.textContent.toLocaleLowerCase() === "mbagathi hospital") {
+    list.classList.add("selected");
+  }
+  list.addEventListener("click", function () {
+    
+    for (let list of selectlist) {
+      if (list.classList.contains("selected")) {
+        list.classList.remove("selected");
+      }
+    }
+    list.classList.add("selected")
+    const listName=list.textContent.toLocaleLowerCase()
+    if (listName==="mbagathi"){
+        for(let category of serviceCategories){
+            if(category.classList.contains("mbagathi")){
+                    category.computedStyleMap.display = "flex"
+            }
+        }
+    }
+
+
+  });
+}
+// tab.classList.add("active");
+//     const tabName=tab.textContent.toLowerCase()
+//     if(tabName==="margherita"){
+//         for (let category of pizzaCategories) {
+//   if (category.classList.contains("margherita")) {
+//     category.style.display = "flex";
+//   } else {
+//     category.style.display = "none";
 //   }
-//   tab.addEventListener("click", function () {
-//     console.log(tab.textContent);
-//     for (let tab of selectTabs) {
-//       if (tab.classList.contains("active")) {
-//         tab.classList.remove("active");
-//       }
+// }
 //     }
+//     else if(tabName==="pepperoni"){
+//         for (let category of pizzaCategories) {
+//   if (category.classList.contains("pepperoni")) {
+//     category.style.display = "flex";
+//   } else {
+//     category.style.display = "none";
+//   }
+// }
+//     }
+//     else if(tabName==="chicken pizza"){
+//   for (let category of pizzaCategories) {
+//   if (category.classList.contains("chicken_pizza")) {
+//     category.style.display = "flex";
+//   } else {
+//     category.style.display = "none";
+//   }
+// }
+//     }
+//     else if(tabName==="meat lover's pizza"){
+       
+//         for (let category of pizzaCategories) {
+//   if (category.classList.contains("meat")) {
+//     category.style.display = "flex";
+//   } else {
+//     category.style.display = "none";
+//   }
+// }
+//     }
+//      else if(tabName==="hawaiian"){
+       
+//         for (let category of pizzaCategories) {
+//   if (category.classList.contains("hawaiian")) {
+//     category.style.display = "flex";
+//   } else {
+//     category.style.display = "none";
+//   }
+// }
+//     }
+ 
+
+
