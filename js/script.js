7f340f58e70ddf3732c553aa77c82712a6929060
feeloochieng-ux <feeloochieng@gmail.com>
2026-06-04 13:59:59 +0300

    updated all buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-const selectTabs = document.querySelectorAll(".list")
+const selectTabs = document.querySelectorAll(".list");
 
 const services = document.querySelectorAll(".content");
+
+for (let service of services) {
+  if (service.classList.contains("mbaga")) {
+    service.style.display = "flex";
+  } else {
+    service.style.display = "none";
+  }
+}
