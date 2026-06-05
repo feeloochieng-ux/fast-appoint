1286e2650beee3103959e78d69402b1a78622c02
feeloochieng-ux <feeloochieng@gmail.com>
2026-06-05 05:49:49 +0300

    updated access to hospital details

--- js/script.js
+++ js/script.js
@@ -10,50 +10,50 @@ for (let category of serviceCategories) {
   }
 }
 
-// for (let list of selectList) {
-//   if (list.textContent.toLowerCase() === "mbagathi hospital") {
-//     list.classList.add("selected");
-//   }
-//   list.addEventListener("click", function () {
-//     for (let list of selectList) {
-//       if (list.classList.contains("selected")) {
-//         list.classList.remove("selected");
-//       }
-//     }
-//     list.classList.add("selected");
-//     const listName = list.textContent.toLowerCase();
-//     if (listName === "mbagathi hospital") {
-//       for (let category of serviceCategories) {
-//         if (category.classList.contains("mbagathi")) {
-//           category.style.display = "flex";
-//         } else {
-//           category.style.display = "none";
-//         }
-//       }
-//     } else if (listName === "makadara hospital") {
-//       for (let category of serviceCategories) {
-//         if (category.classList.contains("makadara")) {
-//           category.style.display = "flex";
-//         } else {
-//           category.style.display = "none";
-//         }
-//       }
-//     } else if (listName === "kenyatta hospital") {
-//       for (let category of serviceCategories) {
-//         if (category.classList.contains("kenyatta")) {
-//           category.style.display = "flex";
-//         } else {
-//           category.style.display = "none";
-//         }
-//       }
-//     } else if (listName === "Kibera-south hospital") {
-//       for (let category of serviceCategories) {
-//         if (category.classList.contains("kibra")) {
-//           category.style.display = "flex";
-//         } else {
-//           category.style.display = "none";
-//         }
-//       }
-//     }
-//   });
-// }
+for (let list of selectList) {
+  if (list.textContent.toLowerCase() === "mbagathi hospital") {
+    list.classList.add("selected");
+  }
+  list.addEventListener("click", function () {
+    for (let list of selectList) {
+      if (list.classList.contains("selected")) {
+        list.classList.remove("selected");
+      }
+    }
+    list.classList.add("selected");
+    const listName = list.textContent.toLowerCase();
+    if (listName === "mbagathi hospital") {
+      for (let category of serviceCategories) {
+        if (category.classList.contains("mbagathi")) {
+          category.style.display = "flex";
+        } else {
+          category.style.display = "none";
+        }
+      }
+    } else if (listName === "makadara hospital") {
+      for (let category of serviceCategories) {
+        if (category.classList.contains("makadara")) {
+          category.style.display = "flex";
+        } else {
+          category.style.display = "none";
+        }
+      }
+    } else if (listName === "kenyatta hospital") {
+      for (let category of serviceCategories) {
+        if (category.classList.contains("kenyatta")) {
+          category.style.display = "flex";
+        } else {
+          category.style.display = "none";
+        }
+      }
+    } else if (listName === "Kibera-south hospital") {
+      for (let category of serviceCategories) {
+        if (category.classList.contains("kibra")) {
+          category.style.display = "flex";
+        } else {
+          category.style.display = "none";
+        }
+      }
+    }
+  });
+}
