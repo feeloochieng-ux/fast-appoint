3f02aba3bd790857c7e88f157a806e783145d83d
feeloochieng-ux <feeloochieng@gmail.com>
2026-06-05 14:14:41 +0300

    added home icon

--- patient`s.js
+++ patient`s.js
@@ -1,5 +1,5 @@
 const formPatient=document.querySelector(".page1")
-const formPatient = document.querySelector(".buttons");
+const formPatient = document.querySelectorAll(".buttons");
 formPatient.addEventListener("click", function () {
     window.open("patient`s.html")
 });
